748f8fe36ac6ee4d81e4d84f723f679701bb4bf7
Harald Prokop <prokop@einblick.ai>
2022-09-23 13:24:16 -0400

    Try subroutine:

---
+++ node/build/npm_run_multiple.sh
@@ -0,0 +1,8 @@
+rest=$1
+while [ -n "$rest" ] ; do
+   str=${rest%%,*}  # Everything up to the first ';'
+   # Trim up to the first ',' -- and handle final case, too.
+   [ "$rest" = "${rest/,/}" ] && rest= || rest=${rest#*,}
+   echo "+ \"$str\"" 
+   npm run $str
+done
